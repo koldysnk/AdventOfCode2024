0d881976f6a442da884815d154e72feabe6b2c7b
koldysnk <koldysnk@dukes.jmu.edu>
2024-12-02 00:16:17 -0500

    Day 2 complete

---
+++ day02part2.py
@@ -0,0 +1,39 @@
+import math
+
+def testSafe(row, dir = 0):
+    safe = True
+
+    for i, x in enumerate(row[:-1]):
+        y = row[i+1]
+
+        diff = x - y
+
+        absDiff = abs(diff)
+        
+        if absDiff < 1 or absDiff > 3:
+            if safe:
+                safe = False
+                continue
+            return False
+
+        if dir == 0:
+            dir = diff//absDiff
+        elif dir != diff//absDiff:
+            if safe:
+                safe = False
+                continue
+            return False
+
+    return True
+
+data = []
+with open("data/day02.txt","r") as f:
+    data = [[int(x) for x in line.strip().split(" ")] for line in f.readlines()]
+
+
+total = 0
+
+for row in data:
+    total += testSafe(row)
+
+print(total)

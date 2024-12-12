f12c018d443b7235d450ff79b0c74c6367a3cd25
koldysnk <koldysnk@dukes.jmu.edu>
2024-12-12 15:13:07 -0500

    Display path

--- day10part2.py
+++ day10part2.py
@@ -1,13 +1,24 @@
 
+def displayGraph(data,visited:set):
+
+    for y, row in enumerate(data):
+        for x, spot in enumerate(row):
+            if (x,y) in visited:
+                print(spot,end="")
+            else:
+                print(".",end="")
+        print()
+    print()
+
 def getScore(data,x,y,spot=0, visited:set = set()):
     if x<0 or y<0 or y>=len(data) or x>=len(data[y])  or (x,y) in visited or data[y][x]!=spot:
         return 0
-    
+    total = 0
+    visited.add((x,y))
+    #displayGraph(data,visited)
     if spot == 9:
         return 1
 
-    total = 0
-    visited.add((x,y))
 
     total += getScore(data,x-1,y,spot+1,visited.copy())
     total += getScore(data,x,y-1,spot+1,visited.copy())

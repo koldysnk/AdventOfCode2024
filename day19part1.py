b7805bc4c6e7f78aa4a81638c1ddc6262baa8788
koldysnk <koldysnk@dukes.jmu.edu>
2025-03-24 17:30:17 -0400

    day 19 part 1

--- day19part1.py
+++ day19part1.py
@@ -7,57 +7,27 @@ def parseData():
 
 patterns, designs = parseData()
 pat_set = set(patterns)
+winners = set()
 
-goods = set()
-bads = set()
+for i, design in enumerate(designs):
+    print(f"Starting pattern {i} - {design}")
 
-count = 0
-for i,design in enumerate(designs):
-    potentials = [(design,"")]
-    good = False
-    options = set()
-    while len(potentials)>0:
-        current_design,past = potentials.pop()
-
-        if current_design == "" or current_design in goods:
-            count+=1
-            good = True
-            goods.add(design)
-            if design not in pat_set:
-                patterns.append(design)
-                pat_set.add(design)
-            #print(f"{i} - {design} - {count}")
-            break
-
-        if current_design in bads:
-            continue
-            #200(low) when using break
-            #219(high) when using continue
-            #compare outputs of each and investigate the difference
-
-        found = False
-        for pattern in patterns:
-            if current_design.startswith(pattern):
-                found = True
-                next = current_design.lstrip(pattern)
-                new_past = past+pattern
-                if new_past not in pat_set:
-                    patterns.append(new_past)
-                    pat_set.add(new_past)
-                if next not in options:
-                    options.add(next)
-                    j = 0
-                    while j<len(potentials) and len(next)<len(potentials[j][0]):
-                        j+=1
-                    potentials.insert(j,(next,new_past))
-        if not found:
-            bads.add(current_design)
-
-
-    if not good:
-        bads.add(design)
-    print(f"{i} - {'good' if good else 'bad'} - {design} - {count}")
-print(count)
-
-#200 is too low
-#219 is too high
+    queue = [('',design)]
+    searching = True
+    while len(queue)>0 and searching:
+        hist, rem = queue.pop()
+        if rem == '':
+            winners.add(design)
+            print(f"Pattern {i} is good ({len(winners)})")
+            searching = False
+        else:
+            paths = set()
+            for pattern in pat_set:
+                if rem.startswith(pattern):
+                    new_hist = hist+pattern
+                    queue.append((new_hist,rem[len(pattern):]))
+                    paths.add(new_hist)
+            pat_set.update(paths)
+    if searching:
+        print(f"Pattern {i} is bad ({len(winners)})")
+print(len(winners))
